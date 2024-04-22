e52765ce76f3c475e171638487760b1fdd273efa
vaporyhumo <roanvilina@gmail.com>
2024-04-21 22:00:11 -0400

    Init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![warn(clippy::complexity)]
+#![warn(clippy::expect_used)]
+#![warn(clippy::nursery)]
+#![warn(clippy::panic)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::perf)]
+#![warn(clippy::unwrap_used)]
+
+fn main() {
+  println!("Hello, world!");
+}
