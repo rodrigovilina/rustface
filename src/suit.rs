9cce5556b94e6810b0f0f0643e13f2a40d4d472e
Rodrigo Vilina <roanvilina@gmail.com>
2025-04-13 00:12:23 -0400

    Add `suit`

---
+++ src/suit.rs
@@ -0,0 +1,22 @@
+use crate::front_color::FrontColor;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Suit {
+  Club,
+  Diamond,
+  Heart,
+  Spade,
+}
+
+impl Suit {
+  pub const fn all() -> [Self; 4] {
+    [Self::Club, Self::Diamond, Self::Heart, Self::Spade]
+  }
+
+  pub const fn front_color(&self) -> FrontColor {
+    match self {
+      Self::Diamond | Self::Heart => FrontColor::Red,
+      Self::Club | Self::Spade => FrontColor::Black,
+    }
+  }
+}
