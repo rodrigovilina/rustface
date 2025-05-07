b4da0c76c89c167c1c1b5d9f80cf7ad096328a5a
Rodrigo Vilina <roanvilina@gmail.com>
2025-05-06 21:08:32 -0400

    Update `discard_pile`

--- src/discard_pile.rs
+++ src/discard_pile.rs
@@ -1,4 +1,4 @@
-use crate::{card::Card, play_pile::PlayPile};
+use crate::card::Card;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct DiscardPile {
@@ -10,33 +10,7 @@ impl DiscardPile {
     Self { cards: vec![] }
   }
 
-  #[allow(dead_code)]
-  pub fn discard(&mut self, play_pile: &mut PlayPile) {
-    self.cards.extend(play_pile.cards());
-    play_pile.discard();
-  }
-}
-
-#[cfg(test)]
-mod tests {
-  use {
-    super::*,
-    crate::{back_color::BackColor, rank::Rank, suit::Suit},
-  };
-
-  #[test]
-  fn test_discard() {
-    let mut play_pile: PlayPile = PlayPile::empty();
-    let mut discard_pile: DiscardPile = DiscardPile::empty();
-    let card = Card::Regular {
-      rank: Rank::Two,
-      suit: Suit::Heart,
-      back_color: BackColor::Red,
-    };
-    play_pile.play(card.clone());
-    discard_pile.discard(&mut play_pile);
-
-    assert_eq!(play_pile.cards(), vec![]);
-    assert_eq!(discard_pile.cards, vec![card.clone()]);
+  pub fn discard(&mut self, cards: Vec<Card>) {
+    self.cards.extend(cards);
   }
 }
